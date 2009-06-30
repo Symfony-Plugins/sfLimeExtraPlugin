1376b1b910f768d5068bd5e33890d775051568f4
bschussek <bschussek@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-06-30 13:49:46 -0500

    Made it possible to comment out annotations with multiline comments
    git-svn-id: http://svn.symfony-project.com/plugins/sfLimeExtraPlugin/trunk@19740 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ test/unit/test/fixtures/test_multiline_comments.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the symfony package.
+ * (c) Bernhard Schussek <bschussek@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+include dirname(__FILE__).'/../../../bootstrap/unit.php';
+
+$t = new lime_test_simple(0);
+
+// @Test
+echo "Test 1\n";
+
+/*
+// @Test
+echo "Test 2\n";
+*/
+
+// @Test
+echo "Test 3\n";
